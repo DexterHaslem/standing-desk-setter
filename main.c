e55877e066a6cf2f2cb96f7c31175fe69dc41d10
Dexter Haslem <dmh@fastmail.com>
2021-07-13 03:43:31 -0600

    initial, post mcc generation

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "mcc_generated_files/mcc.h"
+
+void main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+
+    // Enable the Global Interrupts
+    INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    INTERRUPT_PeripheralInterruptEnable();
+    
+    while (1)
+    {
+        
+    }
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=standing-desk-thing.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/standing-desk-thing.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=standing-desk-thing.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/standing-desk-thing.x.tar
