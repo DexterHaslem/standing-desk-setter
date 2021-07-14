1afe2fe1eef952d4644f7db9a4a370239956e5fe
Dexter Haslem <dmh@fastmail.com>
2021-07-13 18:05:10 -0600

    add vl53l1x distance mode and timing budget config:
    this is the absolutely worst I2C interface and 'use'
    of I2C registers i've EVER seen in my entire career
    and by a very large margin. whoever designed the timing
    budget and distance mode registers was not in this
    spacetime we share. VL53L1X is a memorable name
    after all
    
    having to set several registers to random magical values
    and depending on the distance mode/timing budget instead
    of simply writing the desired values to a single register
    is mysterious. if this was a hobbyists first time adding
    a control mcu to a sensor and interfacing with i2c or
    something that'd be a different story. but this is an
    expensive, long running STsensor that says state of the
    art in the data sheet. it very may well be with its 940nm
    invisible laser but it's hard to care when the interface
    is this so insanely bad to use. I can only imagine what
    kinda of internal cruft and years of legacy ineptitude
    is in the way (VL53L0X prob?) from making this register
    set not total ass to use.
    
    slightly annoyed but its done. now i can set fast mode :3

--- vl53l1x.c
+++ vl53l1x.c
@@ -9,10 +9,20 @@
 #include "mcc_generated_files/i2c1_master.h"
 #include "mcc_generated_files/examples/i2c1_master_example.h"
 
-#define I2C_ADDR (0x29)
+/* address 0x29 over the wire after r/w bit shift */
+#define I2C_ADDR (0x52 >> 1)
 
 #define INIT_TIMEOUT_MS (150)
 
+
+#define PHASECAL_CONFIG__TIMEOUT_MACROP     (0x004B)
+#define RANGE_CONFIG__VCSEL_PERIOD_A        (0x0060)
+#define RANGE_CONFIG__VCSEL_PERIOD_B        (0x0063)
+#define RANGE_CONFIG__VALID_PHASE_HIGH      (0x0069)
+#define SD_CONFIG__WOI_SD0                  (0x0078)
+#define SD_CONFIG__INITIAL_PHASE_SD0        (0x007A)
+#define RANGE_CONFIG__TIMEOUT_MACROP_A_HI   (0x005E)
+#define RANGE_CONFIG__TIMEOUT_MACROP_B_HI  	(0x0061)
 
 /* defines borrowed from vl53l1x_class.cpp - see LICENSES */
 
@@ -176,9 +186,10 @@ static void write2_reg2(uint16_t reg, uint16_t val)
 {
     /* have to swap endian for over the wire */
     uint16_t reg_swapped = (reg>>8) | (reg<<8);
+    uint16_t val_swapped = (val>>8) | (val<<8);
     
     while(!I2C1_Open(I2C_ADDR)); 
-    I2C1_SetDataCompleteCallback(data16_write, &val);
+    I2C1_SetDataCompleteCallback(data16_write, &val_swapped);
     I2C1_SetBuffer(&reg_swapped, 2);
     I2C1_SetAddressNackCallback(NULL,NULL); 
     I2C1_MasterWrite();
@@ -234,10 +245,9 @@ bool vl53l1x_init(void)
     uint8_t data_ready = 0;
     uint8_t timeout = 0;
     
-    /* bit of a doozy to initialize the sensor */
+    /* bit of a doozy to initialize the sensor with static init table */
     for (uint8_t reg_addr = 0x2d; reg_addr <= 0x87; ++reg_addr)
     {
-        //I2C1_Write1ByteRegister(I2C_ADDR, reg_addr, default_cfgs[reg_addr - 0x2d]);
         write1_reg2(reg_addr, default_cfgs[reg_addr - 0x2d]);
     }
     
@@ -270,4 +280,122 @@ uint16_t vl53l1x_get_dist(void)
 {
     uint16_t dist = read2_reg2(0x0096);
     return dist;
+}
+
+/* if you thought this sensor sucked to work with, wait till you see this 
+ range mode and timing budget crap!
+ HOPE YOU LIKE RANDOM MAGICVALS IN SEVERAL REGISTERS */
+
+/* TODO: datasheet has some mid range mode in spec tables.. what */
+
+/* Predefined values = 15, 20, 33, 50, 100(default), 200, 500. */
+
+/*sets distance mode: 1 = short, 2 = long. default is long mode
+ short mode 1.3m range, long up to 4 meters w/ a 200ms timing budget */
+void vl53l1x_set_dist_mode(enum eVL53L1X_DIST_MODE dm)
+{
+    if (dm == VL53L1X_DIST_MODE_UNKNOWN)
+        return;
+    bool sr = dm == VL53L1X_DIST_MODE_SHORT;
+    write1_reg2(PHASECAL_CONFIG__TIMEOUT_MACROP, sr ? 0x14 : 0x0a);
+    write1_reg2(RANGE_CONFIG__VCSEL_PERIOD_A, sr ? 0x07 : 0x0f);
+    write1_reg2(RANGE_CONFIG__VCSEL_PERIOD_B, sr ? 0x05 : 0x0d);
+    write1_reg2(RANGE_CONFIG__VALID_PHASE_HIGH, sr ? 0x38 : 0xb8);
+    write2_reg2(SD_CONFIG__WOI_SD0, sr ? 0x0705 : 0x0f0d);
+    write2_reg2(SD_CONFIG__INITIAL_PHASE_SD0, sr ? 0x0606 : 0x0e0e);
+}
+
+enum eVL53L1X_DIST_MODE vl53l1x_get_dist_mode(void)
+{
+    enum eVL53L1X_DIST_MODE ret = VL53L1X_DIST_MODE_UNKNOWN;
+    uint8_t v = read1_reg2(PHASECAL_CONFIG__TIMEOUT_MACROP);
+    /* you think it would kill them to use a register with actual values */
+    if (v == 0x14)
+    {
+        ret = VL53L1X_DIST_MODE_SHORT;
+    }
+    else if (v == 0x0a)
+    {
+        ret = VL53L1X_DIST_MODE_LONG;
+    }
+    
+    return ret;
+}
+
+uint16_t vl53l1x_get_timing_budget_ms(void)
+{
+    uint16_t v = read2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_A_HI);
+    switch(v)
+    {
+        case 0x001d:
+            return 15;
+        case 0x0051:
+        case 0x001e:
+            return 20;
+        case 0x00d6:
+        case 0x0060:
+            return 33;
+        case 0x01ae:
+        case 0x00ad:
+            return 50;
+        case 0x02e1:
+        case 0x01cc:
+            return 100;
+        case 0x03e1:
+        case 0x02d9:
+            return 200;
+        case 0x0591:
+        case 0x048f:
+            return 500;
+    }
+    return 0;
+}
+
+void vl53l1x_set_timing_budget_ms(uint16_t tb)
+{
+    /* once again for some mysterious reason, magic numbers in registers depending 
+        on pre values, and even worse, depending on distance mode too.
+        in an alternate universe this would be an unconditional single register write. 
+        * this is currently the worst i2c register use i've seen in my career  */
+    
+    enum eVL53L1X_DIST_MODE dm = vl53l1x_get_dist_mode();
+    if (dm == VL53L1X_DIST_MODE_UNKNOWN)
+        return;
+    
+    bool lr = dm == VL53L1X_DIST_MODE_LONG;
+    switch (tb)
+    {
+        case 15:
+            /* 15 is only available in short distance mode .. */
+            if (!lr)
+            {
+                write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_A_HI, 0x001d);
+                write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_B_HI, 0x0027);
+            }
+            break;
+        case 20:
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_A_HI, lr ? 0x001e : 0x0051);
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_B_HI, lr ? 0x0022 : 0x006e);
+            break;
+        case 33:
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_A_HI, lr ? 0x0060 : 0x00d6);
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_B_HI, lr ? 0x006e : 0x006e);
+            break;
+        case 50:
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_A_HI, lr ? 0x00ad : 0x01ae);
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_B_HI, lr ? 0x00c6 : 0x01e8);
+            break;
+        case 100:
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_A_HI, lr ? 0x01cc : 0x02e1);
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_B_HI, lr ? 0x01ea : 0x0388);
+            break;
+        case 200:
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_A_HI, lr ? 0x02d9 : 0x03e1);
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_B_HI, lr ? 0x02f8 : 0x0496);
+            break;        
+        case 500:
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_A_HI, lr ? 0x048f : 0x0591);
+            write2_reg2(RANGE_CONFIG__TIMEOUT_MACROP_B_HI, lr ? 0x04a4 : 0x05c1);
+            break;
+    }
 }
